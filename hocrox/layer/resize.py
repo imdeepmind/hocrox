5d5be29e8734f89419f5453dd4b155d8c2924b80
Abhishek Chatterjee imdeepmind <abhishek.chatterjee97@protonmail.com>
2021-11-14 18:03:46 +0530

    added Resize layer, summary method

---
+++ hocrox/layer/resize.py
@@ -0,0 +1,21 @@
+import cv2
+
+
+class Resize:
+    def __init__(self, dim, interpolation, name):
+        self.__dim = dim
+        self.__interpolation = interpolation
+        self.__name = name
+        self.__type = "resize"
+
+    def apply_layer(self, img):
+        return cv2.resize(img, self.__dim, self.__interpolation)
+
+    def get_description(self):
+        return (
+            f"{self.__name}({self.__type})",
+            f"Dim: {self.__dim}, Interpolation: {self.__interpolation}",
+        )
+
+    def get_layer(self):
+        return self
--- hocrox/model/sequential.py
+++ hocrox/model/sequential.py
@@ -1,18 +1,44 @@
+from prettytable import PrettyTable
+
+
 class Sequential:
     def __init__(self):
-        pass
+        self.__frozen = False
+        self.__layers = []
 
-    def add(self):
-        pass
+    def add(self, layer):
+        if self.__frozen:
+            raise ValueError("Model is frozen")
+
+        self.__layers.append(layer)
 
     def summary(self):
-        pass
+        t = PrettyTable(["Index", "Name", "Parameters"])
+
+        for index, layer in enumerate(self.__layers):
+            (
+                name,
+                parameters,
+            ) = layer.get_description()
+
+            t.add_row(
+                [
+                    f"#{index+1}",
+                    name,
+                    parameters,
+                ]
+            )
+
+        return str(t)
 
     def transform(self):
         pass
 
     def transform_generator(self):
         pass
+
+    def freeze(self):
+        self.__frozen = True
 
     def save(self):
         pass
