404fcdf8bf5e7c3a5c02393a99d020917548288e
Abhishek Chatterjee imdeepmind <abhishek.chatterjee97@protonmail.com>
2021-11-24 22:56:05 +0530

    added method to check if layer is valid or not

--- hocrox/utils/is_valid_layer.py
+++ hocrox/utils/is_valid_layer.py
@@ -1,2 +1,20 @@
 def is_valid_layer(layer):
+    if not hasattr(layer, "get_description"):
+        return False
+
+    if not hasattr(layer, "get_name"):
+        return False
+
+    if not hasattr(layer, "get_type"):
+        return False
+
+    if not hasattr(layer, "get_supported_parent_layer"):
+        return False
+
+    if not hasattr(layer, "get_bypass_validation"):
+        return False
+
+    if not hasattr(layer, "apply_layer"):
+        return False
+
     return True
