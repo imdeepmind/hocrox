9ed86c6d347807163ff5526c4b134259aa273191
Abhishek Chatterjee imdeepmind <abhishek.chatterjee97@protonmail.com>
2021-11-25 22:49:49 +0530

    updated the  vertical flip layer

--- hocrox/layer/preprocessing/vertical_flip.py
+++ hocrox/layer/preprocessing/vertical_flip.py
@@ -1,8 +1,10 @@
 """VerticalFlip layer for Hocrox."""
 import cv2
 
+from hocrox.utils import Layer
 
-class VerticalFlip:
+
+class VerticalFlip(Layer):
     """Rotate layer for Hocrox."""
 
     def __init__(self, name=None):
@@ -11,25 +13,23 @@ class VerticalFlip:
         :param name: name of the layer
         :type name: str
         """
-        if name and not isinstance(name, str):
-            raise ValueError(f"The value {name} for the argument name is not valid")
-
-        self.__name = name if name else "VerticalFlip Layer"
-
-        self.type = "vertical_flip"
-        self.supported_parent_layer = [
-            "resize",
-            "greyscale",
-            "rotate",
-            "crop",
-            "padding",
-            "save",
-            "horizontal_flip",
+        super().__init__(
+            name,
             "vertical_flip",
-            "random_rotate",
-            "random_flip",
-        ]
-        self.bypass_validation = False
+            [
+                "resize",
+                "greyscale",
+                "rotate",
+                "crop",
+                "padding",
+                "save",
+                "horizontal_flip",
+                "vertical_flip",
+                "random_rotate",
+                "random_flip",
+            ],
+            "-",
+        )
 
     def apply_layer(self, images, name=None):
         """Apply the transformation method to change the layer.
@@ -45,11 +45,3 @@ class VerticalFlip:
             transformed_images.append(cv2.flip(image, 0))
 
         return transformed_images
-
-    def get_description(self):
-        """Return layers details for the model to generate summary.
-
-        :return: layer details
-        :rtype: str
-        """
-        return (f"{self.__name}({self.type})", "-")
