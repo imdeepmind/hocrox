5323733b0b157986dde12a12c2f68e0a3a9b76e1
Abhishek Chatterjee imdeepmind <abhishek.chatterjee97@protonmail.com>
2021-12-03 01:20:37 +0530

    updated the Layer class doc

--- hocrox/utils/layer.py
+++ hocrox/utils/layer.py
@@ -2,27 +2,77 @@
 
 
 class Layer:
-    """Layer class is used to make layers for Hocrox."""
+    """Layer class is used to make layers for Hocrox.
+
+    Here is an example code for making a new custom CropCustom Hocrox layer using this Layer class as basedclass.
+
+    ```python
+    from hocrox.utils import Layer
+
+    class CropCustom(Layer):
+        def __init__(self, x, y, w, h, name=None):
+            if x and not isinstance(x, int):
+                raise ValueError(f"The value {x} for the argument x is not valid")
+
+            if y and not isinstance(y, int):
+                raise ValueError(f"The value {y} for the argument y is not valid")
+
+            if w and not isinstance(w, int):
+                raise ValueError(f"The value {w} for the argument w is not valid")
+
+            if h and not isinstance(h, int):
+                raise ValueError(f"The value {h} for the argument h is not valid")
+
+            self.__x = x
+            self.__y = y
+            self.__w = w
+            self.__h = h
+
+            super().__init__(
+                name,
+                "crop",
+                [
+                    "resize",
+                    "greyscale",
+                    "rotate",
+                    "crop",
+                    "padding",
+                    "save",
+                    "horizontal_flip",
+                    "vertical_flip",
+                    "random_rotate",
+                    "random_flip",
+                ],
+                f"X: {self.__x}, Y: {self.__y}, W: {self.__w}, H: {self.__h}",
+            )
+
+        def _apply_layer(self, images, name=None):
+            transformed_images = []
+
+            for image in images:
+                transformed_images.append(image[self.__x : self.__x + self.__w, self.__y : self.__y + self.__h])
+
+            return transformed_images
+    ```
+    """
 
     def __init__(self, name, type, supported_parent_layer, parameter_str, bypass_validation=False):
-        """Init method for the Layer class.
+        """Init method for Layer class.
 
-        Arguments:
-            name {str} -- Name of the layer
-            type {str} -- Type of the layer
-            supported_parent_layer {list} -- List of supported parent layers
-            parameter_str {str} -- The parameter string for printing the model summary
-
-        Keyword Arguments:
-            bypass_validation {bool} -- Flag to bypass layer validation, useful when making
-            custom layers (default: {False})
+        Args:
+            name (str): Name of the layer.
+            type (str): Type of the layer
+            supported_parent_layer (list): List of layers that the current layers supports as parent.
+            parameter_str (str): Parameter string used for model summary generation.
+            bypass_validation (bool, optional): Flag to bypass validation flags in model.add() function. Used heavy
+                when making new custom layers. Defaults to False.
 
         Raises:
-            ValueError: If the name is not valid
-            ValueError: If the type is not valid
-            ValueError: If the supported_parent_layer is not valid
-            ValueError: If the parameter_str is not valid
-            ValueError: If the bypass_validation is not valid
+            ValueError: If the name parameter is not valid.
+            ValueError: If the type parameter is not valid.
+            ValueError: If the supported_parent_layer parameter is not valid.
+            ValueError: If the parameter_str parameter is not valid.
+            ValueError: If the bypass_validation parameter is not valid.
         """
         if name and not isinstance(name, str):
             raise ValueError(f"The value {name} for the argument name is not valid")
@@ -46,10 +96,12 @@ class Layer:
         self.__bypass_validation = bypass_validation
 
     def _get_description(self):
-        """Return the description tuple of the layer, used in printing the model summary.
+        """Return the description string of the layer.
+
+        Used for generating model summary.
 
         Returns:
-            tuple -- Description of the layer
+            tuple: Description of the layer.
         """
         return (f"{self.__name}({self.__type})", self.__parameter_str)
 
@@ -57,10 +109,10 @@ class Layer:
         """Check if based on the previous layer type, the current is valid or not.
 
         Arguments:
-            previous_layer_type {str} -- Type of the previous layer
+            previous_layer_type (str): Type of the previous layer
 
         Returns:
-            bool -- True if the layer is valid, else False
+            bool: True if the layer is valid, else False
         """
         if self.__bypass_validation:
             return True
@@ -71,7 +123,7 @@ class Layer:
         """Return the name of the layer.
 
         Returns:
-            str -- Name of the layer
+            str: Name of the layer
         """
         return self.__name
 
@@ -79,6 +131,6 @@ class Layer:
         """Return the type of the layer.
 
         Returns:
-            str -- Type of the layer
+            str: Type of the layer
         """
         return self.__type
