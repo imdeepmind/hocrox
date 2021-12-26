e097babf579f678fe189584f71c0a7308f0b0423
Abhishek Chatterjee <abhishek.chatterjee97@protonmail.com>
2021-12-26 14:52:13 +0530

    minor changes to the docs

--- hocrox/layer/augmentation/__init__.py
+++ hocrox/layer/augmentation/__init__.py
@@ -5,8 +5,6 @@ In other words, Augmentation means artificially creating more data to train ML o
 In image Augmentation, we slightly rotate, flip, change the color of the images to generate multiple versions of the
 image. Since all these images are from the original image with slight modifications, they still represent
 the original patterns from the original images (most of the time).
-
-In Hocrox, currently, there are only 2 augmentation layers, RandomFlip and RandomRotate.
 """
 
 from .random_rotate import RandomRotate
